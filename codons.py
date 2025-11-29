3c11bd11d0f2fe82b58d7ff7835fac42e7c00b8f
ofersomberg-dev <ofersomberg@gmail.com>
2025-11-29 19:23:56 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,16 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+              def create_codon_dict(file_path):
+    mapping_between_codons_and_amino_acids = {}
+    with open(file_path, 'r') as f:
+        for line in f:
+            if not line.strip():
+                continue
+            cells = line.strip().split('\t') # Changed 'row' to 'line'
+            if len(cells) < 3:
+                continue
+            codon = cells[0]
+            amino_acid = cells[2]
+            mapping_between_codons_and_amino_acids[codon] = amino_acid
+    return mapping_between_codons_and_amino_acids
 
 
